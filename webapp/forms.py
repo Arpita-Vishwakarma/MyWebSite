0f2cb73bbc4631138572f3b9b2b51eb560951dbe
Arpita Vishwakarma <varpita974@gmail.com>
2024-07-19 16:37:04 +0530

    form

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class UserInfoForm(forms.Form):
     name = forms.CharField(max_length=100, required=True,
-    widget=forms.TextInput(attrs={'class': 'contact__input', 'placeholder': 'name'}))
+    widget=forms.TextInput(attrs={'class': 'contact__input'}))
     email = forms.EmailField(required=True,
-    widget=forms.EmailInput(attrs={'class': 'contact__input', 'placeholder': 'example@gmail.com'}))
+    widget=forms.EmailInput(attrs={'class': 'contact__input'}))
     message = forms.CharField(required=True,
-    widget=forms.Textarea(attrs={'class': 'contact__input', 'placeholder': 'Message', 'cols': 30, 'rows': 10}))
+    widget=forms.Textarea(attrs={'class': 'contact__input', 'cols': 30, 'rows': 10}))
